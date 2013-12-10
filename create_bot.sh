13c7bee00ff20561f8f2fcc6613568f0a3c12a4a
Ceili Cornelison <ceili@ceilicornelison.com>
2013-12-10 00:50:46 -0600

    Clearly I should just do it in php. This is messy but works, now to refactor

--- create_bot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source private_vars.sh
-
-START=''"'"'{"bot":{"name":"@CoMoNSCoder","group_id":"'
-MIDDLE='"}}'"'"' https://api.groupme.com/v3/bots?token='
-echo curl -X POST -d $START$GroupID$MIDDLE$AccessToken
