629f7c3b2d60c60b5196b760cf8c3f88f4b3574c
Ceili Cornelison <ceili@ceilicornelison.com>
2013-12-10 00:51:38 -0600

    Don't need this anymore - doing it with php.

--- private_vars.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#Your access token can be found here: https://dev.groupme.com/bots
-AccessToken=youraccesstoken
-
-#Your group id can be found by running this curl script: curl https://api.groupme.com/v3/groups?token=youraccesstoken
-GroupID=yourgroupid
