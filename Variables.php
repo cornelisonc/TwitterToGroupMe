d1ca394dbe8e495ea49dfdafacd3a8169dd7192a
Ceili Cornelison <ceili@ceilicornelison.com>
2013-12-10 01:04:28 -0600

    Default vars for public distribution

---
+++ Variables.php
@@ -0,0 +1,14 @@
+<?php
+
+// GroupMe Vars:
+// Your access token can be found here: https://dev.groupme.com/bots
+$AccessToken                ='youraccesstoken';
+
+// Your group id can be found by running this curl script: curl https://api.groupme.com/v3/groups?token=youraccesstoken
+$GroupID                    ='yourgroupid';
+
+// These all come from your Twitter app:
+$oauth_access_token         = 'yourtwitteroauthaccesstoken';
+$oauth_access_token_secret  = 'yoursecretoauthtwitteraccesstoken';
+$consumer_key               = 'yourconsumerkey';
+$consumer_secret            = 'yoursecretconsumerkey';
