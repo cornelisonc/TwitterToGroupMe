2626a0708e6f5a728ac4463ddcf32a57fa690b34
Ceili Cornelison <ceili@ceilicornelison.com>
2013-12-16 13:01:06 -0600

    Final time manipulation

--- GetTweets.php
+++ GetTweets.php
@@ -4,10 +4,10 @@ require_once('TwitterAPIExchange.php');
 require_once('Variables.php');
 
 $settings = array(
-	'oauth_access_token' 		=> $oauth_access_token,
-	'oauth_access_token_secret'	=> $oauth_access_token_secret,
-	'consumer_key'				=> $consumer_key,
-	'consumer_secret'			=> $consumer_secret
+        'oauth_access_token'            => $oauth_access_token,
+        'oauth_access_token_secret'     => $oauth_access_token_secret,
+        'consumer_key'                          => $consumer_key,
+        'consumer_secret'                       => $consumer_secret
 );
 
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
@@ -16,9 +16,8 @@ $requestMethod = 'GET';
 
 $twitter = new TwitterAPIExchange($settings);
 $tweet = $twitter->setGetfield($getfield)
-				 ->buildOauth($url, $requestMethod)
-				 ->performRequest();
-
+                                 ->buildOauth($url, $requestMethod)
+                                 ->performRequest();
 $obj = json_decode($tweet);
 
 $tweet_text = ($obj['0']->text);
@@ -26,23 +25,25 @@ $tweet_time = ($obj['0']->created_at);
 
 $five_minutes_ago = strtotime('5 minutes ago');
 
-if ($tweet_time > $five_minutes_ago) {
-	// Start curl posting things
-	$ch = curl_init();
-	$post = json_encode(array(
-		'bot_id' => $bot_id,
-		'text'	 => $tweet_text
-		)
-	);
+echo "Running";
 
-	$arr = array();
-	array_push($arr, 'Content-Type: application/json; charset=utf-8');
+if (strtotime($tweet_time) > $five_minutes_ago) {
+        // Start curl posting things
+        $ch = curl_init();
+        $post = json_encode(array(
+                'bot_id' => $bot_id,
+                'text'   => $tweet_text
+                )
+        );
 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $arr);
-	curl_setopt($ch, CURLOPT_URL, 'https://api.groupme.com/v3/bots/post');
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $arr = array();
+        array_push($arr, 'Content-Type: application/json; charset=utf-8');
 
-	curl_exec($ch);
-	curl_close($ch);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arr);
+        curl_setopt($ch, CURLOPT_URL, 'https://api.groupme.com/v3/bots/post');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+        curl_exec($ch);
+        curl_close($ch);
 }
